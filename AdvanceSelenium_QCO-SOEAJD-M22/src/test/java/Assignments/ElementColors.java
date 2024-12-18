cd031710a6cd958daad73d319397541c4a5912d1
malat <malat@Malathi>
2024-12-18 12:03:24 +0530

    eng1commit

--- AdvanceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/ElementColors.java
+++ AdvanceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/ElementColors.java
@@ -11,7 +11,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ElementColors {
 
 	public static void main(String[] args) {
-		
+		//Pull back
+		//I am Pushing code to GitHub
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.amazon.in");
 		driver.manage().window().maximize();
